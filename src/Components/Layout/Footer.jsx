66d4645ef94533dfde1e3b90f6b469033b87f71f
Atilla-Koz <atillakoz@hotmail.com>
2024-09-10 20:14:47 +0300

    chore: Update contact information in Footer component

--- src/Components/Layout/Footer.jsx
+++ src/Components/Layout/Footer.jsx
@@ -3,12 +3,14 @@ export default function Footer() {
     <section className="">
       <div className="flex flex-row justify-start w-full h-[85px]">
         <section className="flex flex-row justify-center items-center gap-4">
-          <hr className="w-[40px] h-[2px] bg-white"></hr>
+          <hr className="w-[40px]  bg-white"></hr>
           <p className="text-white pr-8">+(+90) 554 696 30 48</p>
-        </section>{' '}
-        <section className="flex flex-row justify-center items-center gap-4 ">
-          <hr className="w-[40px] h-[2px] bg-[#FF264A] "></hr>
-          <p className="text-[#FF264A]">atillakoz@hotmail.com</p>
+        </section>
+        <section className="flex flex-row justify-center items-center gap-4">
+          <hr className="w-[40px]  border-[#FF264A]"></hr>
+          <a href="mailto:atillakoz@hotmail.com" className="text-[#FF264A]">
+            atillakoz@hotmail.com
+          </a>
         </section>
       </div>
     </section>
